c7d56d4db47e074892a6de4a40bb837ba1f64b12
Viktor Kayatkin <kayatkin@mail.ru>
2023-09-23 22:01:30 +0700

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,22 @@
+const timerElement = document.getElementById("timer");
+
+const initialSeconds = parseInt(timerElement.innerText);
+
+const countdownInterval = setInterval(function() {
+    const hours = Math.floor(initialSeconds / 3600);
+    const minutes = Math.floor((initialSeconds % 3600) / 60);
+    const seconds = initialSeconds % 60;
+
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+
+    timerElement.innerText = formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
+
+    initialSeconds--;
+
+    if (initialSeconds < 0) {
+        clearInterval(countdownInterval);
+        alert("Вы победили в конкурсе!");
+    }
+}, 1000);
